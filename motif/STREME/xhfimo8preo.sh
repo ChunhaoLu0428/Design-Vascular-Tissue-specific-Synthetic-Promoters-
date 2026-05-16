a15ad5f45883ef4e736a2a0662240f98bd340b46
ChunhaoLu0428 <127092367+ChunhaoLu0428@users.noreply.github.com>
2026-05-16 16:56:45 +0800

    Add files via upload

---
+++ motif/STREME/xhfimo8preo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 定义基序文件路径
+motif_file="/home/share_data1/luchh/vascular24ts/STREME/100/NRmerged_streme.meme"
+
+# 定义输入的父目录
+input_base_dir="/home/share_data1/luchh/promoter_seq/LM50/"
+
+# 定义基序文件的输出根目录
+fimo_output_base_dir="/home/share_data1/luchh/vascular24ts/STREME/100/fimo_out"
+
+# 遍历所有子目录（遍历 LM50/ 下的所有子目录）
+for sub_dir in "$input_base_dir"/*/; do
+    # 确定当前子目录的名称，例如 "500" 或其他子目录名称
+    sub_dir_name=$(basename "$sub_dir")
+    
+    # 检查每个子目录下的 promoter_output 是否存在 merged_promoters.fa
+    promoter_file="$sub_dir/promoter_output/merged_promoters.fa"
+    
+    if [[ -f "$promoter_file" ]]; then
+        # 设置 fimo 的输出目录，根据子目录名称决定
+        output_dir="$fimo_output_base_dir/$sub_dir_name"
+        
+        # 创建输出目录（如果不存在）
+        mkdir -p "$output_dir"
+        
+        # 执行 fimo 命令
+        echo "Running fimo for $promoter_file in $sub_dir_name..."
+        nohup fimo --oc "$output_dir" "$motif_file" "$promoter_file" &
+    else
+        echo "File $promoter_file does not exist. Skipping..."
+    fi
+done
+
+# 等待所有后台任务完成
+wait
+echo "FIMO analysis completed for all directories."
